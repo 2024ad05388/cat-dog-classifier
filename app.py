6dd66c8366c41ffcbb7103ae8e1b6a07dd1cafaf
2024ad05388 <2024ad05388@wilp.bits-pilani.ac.in>
2026-08-13 00:58:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ uploaded_file = st.file_uploader("Upload a photo of a cat or dog", type=["jpg", 
 
 if uploaded_file is not None:
     img = Image.open(uploaded_file).convert('RGB')
-    st.image(img, caption="Uploaded Image", use_column_width=True)
+    st.image(img, caption="Uploaded Image", use_container_width=True)
 
     img_tensor = transform(img).unsqueeze(0)
     with torch.no_grad():
